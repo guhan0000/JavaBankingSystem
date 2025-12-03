06588a9f935bf9498e6c7ab20c6f9dab83296ba6
guhan0000 <guhanm0000@gmail.com>
2025-12-03 21:11:34 +0530

    edited

---
+++ src/main/java/com/obs/Account.java
@@ -0,0 +1,41 @@
+package com.obs;
+
+public class Account {
+	private long accountNumber;
+	private String fullName;
+	private String email;
+	private double accountBalance;
+	private String accountPin;
+	public long getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	public String getAccountPin() {
+		return accountPin;
+	}
+	public void setAccountPin(String accountPin) {
+		this.accountPin = accountPin;
+	}
+	
+
+}
