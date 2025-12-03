9937788088c50c4cfefbdcbd4aa68edff3fb6244
guhan0000 <guhanm0000@gmail.com>
2025-12-03 21:08:54 +0530

    Login method created

--- src/main/java/com/obs/UserManagement.java
+++ src/main/java/com/obs/UserManagement.java
@@ -8,17 +8,33 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-@Component
+//@Component
 public class UserManagement {
 	@Autowired
 	DataSource dataSource;
 	@Autowired
 	JdbcTemplate jdbcTemplate;
+	@Autowired
+	User user;
+	public UserManagement(User user){
+		this.user=user;
+		
+	}
 	
-	 void createUser(String userEmail,String userName, String userPwd) {
+	 User createUser(String userEmail,String userName, String userPwd) {
 		Object arr[]= {userName,userEmail,userPwd};
 		String sql="INSERT INTO users (full_name,email,password) VALUES (?,?,?)";
-		jdbcTemplate.update(sql,arr);
+		int update = jdbcTemplate.update(sql,arr);
+		if(update==1) {
+			System.out.println(userName+" User created Successfully");
+			user.setUserFullName(userName);
+			user.setUserEmail(userEmail);
+			user.setUserPwd(userPwd);
+			return user;
+			}
+		else {
+			return null;
+		}
 
 	}
 	 User getUser(String userEmail) {
@@ -31,10 +47,19 @@ public class UserManagement {
 					   user.setUserEmail(rs.getString("email"));
 					   user.setUserPwd(rs.getString("password"));
 					   return user;
-				   });
-		 
-		 
-
+				   });}
+	  void userLogin(String email,String password) {
+		// TODO Auto-generated method stub
+		  Object arr[]= {email,password};
+		  String sql="SELECT email FROM users WHERE email=? AND password=?";
+		  String result = jdbcTemplate.queryForObject(sql, String.class,arr);
+//		   if(!result.equals("")){
+//			   System.out.println(result+" Logged in Sucessfully");
+//		 }
+//		   else {
+//			System.out.println("User "+email+" Not Found");
+//		}
+		 System.out.println(result);
 	}
 	
 	
